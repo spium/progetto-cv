12b2fb087f7cb9c75633208d5644ec8f5d7e5a4a
giovanni <gpiumatti89@gmail.com>
2014-07-21 17:32:17 +0200

    Added run script for windows

---
+++ ProgettoCV/run.bat
@@ -0,0 +1,2 @@
+java -Djava.library.path="%PATH%;./lib/OpenCV/;./lib/OpenNI/;./lib/NiTE/" -cp "./bin;./lib/*;./lib/Jena/*;./lib/Jung/*;./lib/NiTE/*;./lib/OpenNI/*;./lib/OpenCV/*" it.polito.computervision.controller.Main %*
+
